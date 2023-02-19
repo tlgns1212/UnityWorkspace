ddfce61f73eaa99616a26100243ba9079957402d
Sihoon Kim <tlgns1212@naver.com>
2023-02-19 23:44:50 +0900

    (23/02/19) Listener, Session, Connector

--- GameServerForUnity/Server/DummyClient/Program.cs
+++ GameServerForUnity/Server/DummyClient/Program.cs
@@ -1,12 +1,67 @@
-﻿using System;
+﻿using ServerCore;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DummyClient
 {
-    internal class Program
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected: {endPoint}");
+             // 보낸다 
+            for (int i = 0; i < 5; i++)
+            {
+                byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World{i}");
+                Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected: {endPoint}");
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Server] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Connecter connector = new Connecter();
+
+            connector.Connect(endPoint, () => { return new GameSession(); });
+
+            while (true)
+            {
+                try
+                {
+                   
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+                Thread.Sleep(100);
+            }
+            
         }
     }
 }
--- GameServerForUnity/Server/Server/Program.cs
+++ GameServerForUnity/Server/Server/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Collections.Specialized.BitVector32;
+using ServerCore;
 
 namespace Server
 {
-    internal class Program
+    class GameSession : Session
     {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected: {endPoint}");
+            byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+            Send(sendBuff);
+            Thread.Sleep(1000);
+            Disconnect();
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected: {endPoint}");
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Client] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+
+    class Program
+    {
+        static Listener _listener = new Listener();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 멀티쓰레드
+            // 한것들 : Thread, ThreadPool, Task, TaskCreationOptions, Thread.MemoryBarrier(), Interlocked, Interlocked.Increment(ref num)
+            // (Monitor.Enter(obj), Monitor.Exit(obj)) -> lock(obj)으로 대체, AutoResetEvent (자동문같은 형식),
+            // SpinLock, ReaderWriterLock, ThreadLocal<string>
+
+
+            // 네트워크 프로그래밍
+            // DNS (Domain Name System), 비동기방식 : Socket.AsyncAccept(), 
+
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+
+
+            _listener.Init(endPoint, () => { return new GameSession(); });
+            Console.WriteLine("Listening...");
+
+            while (true)
+            {
+
+            }
+
         }
     }
 }
