6c1ea46fecf4e89e052bdf591c354ac271a288e9
Sihoon Kim <tlgns1212@naver.com>
2023-03-10 22:37:15 +0900

    (23/03/10) Client & Server Connected(Create, Move)

---
+++ MMO_Game/Common/protoc-3.12.3-win64/bin/GenPackets.bat
@@ -0,0 +1,8 @@
+protoc.exe -I=./ --csharp_out=./ ./Protocol.proto
+IF ERRORLEVEL 1 PAUSE
+
+start ../../../GameServerForUnity/PacketGenerator/bin/PacketGenerator.exe ./Protocol.proto
+xcopy /Y Protocol.cs "../../../Client/Assets/Scripts/Packet"
+xcopy /Y Protocol.cs "../../../GameServerForUnity/Server/Packet"
+xcopy /Y ClientPacketManager.cs "../../../Client/Assets/Scripts/Packet"
+xcopy /Y ServerPacketManager.cs "../../../GameServerForUnity/Server/Packet"
